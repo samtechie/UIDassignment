6e20b3ffb192183e7dc94c7c07660efbc0f79503
Wasswa Samuel <latimerscope@gmail.com>
2012-10-31 21:50:10 +0300

    Initial Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UIDassignment::Application.config.secret_token = '910da42cfda147db509cbe65c44ea2fbe00c8fe08e68e7d48df6a5e2992be5f8bb6ea422e8800ab78069f16c446dc3348d65a0a4a590b25156263a8436d568ea'
